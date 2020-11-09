4a6a4094ad4918d3ceaf0aba7f55ddccca745eb1
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-11-09 16:07:14 +0300

    todos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import * as React from 'react'
 import './App.css'
 
 import Budget from './components/Budget/Budget'
+import Todo from './components/Todo/Todo'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
+        <Todo />
         <Budget />
       </header>
     </div>
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,113 @@
+import randomToken from 'random-token'
+import * as React from 'react'
+
+function todosReducer(state = [], {type, payload}) {
+  switch (type) {
+    case 'add':
+      console.log(state)
+      return {todos: [...state.todos, {id: randomToken(5), value: payload}]}
+    case 'remove':
+      console.log('here')
+      state.todos.splice(payload, 1)
+      return {...state}
+
+    default:
+      return state
+  }
+}
+
+function Todo() {
+  const [todos, setTodos] = React.useState([])
+  const [todo, setTodo] = React.useState('')
+
+  React.useEffect(() => {}, [todos])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (todos?.find(item => item.value === todo)) {
+      setTodo('')
+      return console.log('oooops')
+    } else {
+      setTodos([...todos, {id: randomToken(5), value: todo}])
+      setTodo('')
+    }
+  }
+  return (
+    <>
+      <h1>Todo</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="todo"
+            style={{
+              background: '#6d6d6d33',
+              color: 'white',
+              padding: 5,
+              margin: 5,
+              border: 0,
+            }}
+            value={todo}
+            onChange={e => setTodo(e.target.value)}
+            required
+          />
+          <button
+            style={{
+              background: 'green',
+              color: 'white',
+              padding: 4,
+              margin: 5,
+              border: 0,
+            }}
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            textAlign: 'left',
+            textTransform: 'capitalize',
+            margin: '10px 5px',
+          }}
+        >
+          {todos.map(({id, value}, i) => {
+            return (
+              <div
+                key={id}
+                style={{
+                  display: 'flex',
+                  borderBottom: '1px solid cadetblue',
+                  padding: '2px 4px',
+                  placeContent: 'space-between',
+                }}
+              >
+                <span key={id}>{value}</span>
+                <button
+                  type="button"
+                  onClick={() => {
+                    todos.splice(i, 1)
+                    setTodos([...todos])
+                  }}
+                  style={{
+                    background: 'red',
+                    color: 'white',
+                    border: 0,
+                    fontSize: 20,
+                    margin: 5,
+                    paddingBottom: 3,
+                  }}
+                >
+                  <b>x</b>
+                </button>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Todo
