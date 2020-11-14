097fe7b7efa0556da31a1c36c2198d446aa34c89
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-11-14 16:41:00 +0300

    added msw faker @testing-library/react-hooks >>scripts: coverage test
    moved RTL/jest-dom to devdep

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // '*.js': ['eslint', 'jest --findRelatedTests'],
-  '*.js': ['eslint'],
+  '*.js': ['eslint', 'jest --findRelatedTests'],
   '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
     'prettier --write',
   ],
