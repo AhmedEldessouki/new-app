c5f2391045d76faee369cd4f44ffc1190c03c89d
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2020-11-14 16:44:43 +0300

    test script edit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.js': ['eslint', 'jest --findRelatedTests'],
+  '*.js': ['eslint', 'npm t -- --watchAll=false'],
   '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
     'prettier --write',
   ],
